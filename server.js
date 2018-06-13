c39535ac37a4d3005093355a691ea471e292d04e
Kyle Barr <kylebarr54321@gmail.com>
2018-06-13 18:33:18 -0400

    bot sends messages.

--- server.js
+++ server.js
@@ -13,13 +13,34 @@ let opts = {
   ]
 };
 
-let knownCommands = { heartbeat };
+let knownCommands = { heartbeat, roooooosh };
 
 function heartbeat (target, context, params) {
-  if (params.length) {
-    const msg = 'Yeah, shutup.';
+  const msg = 'Yeah, shutup.';
 
-    sendMessage(target, context, msg)
+  sendMessage(target, context, msg);
+}
+
+function roooooosh (target, context) {
+  const msgs = [
+    '/me bans roooooosh forever',
+    'roooooosh is the best person ever!',
+    '/me gives roooooosh a plush stitch'
+  ]
+
+  const number = Math.floor(Math.random() * (2 + 1));
+
+  const msg = msgs[number];
+
+  sendMessage(target, context, msg);
+}
+
+// Helper function to send the correct type of message:
+function sendMessage (target, context, message) {
+  if (context['message-type'] === 'whisper') {
+    client.whisper(target, message)
+  } else {
+    client.say(target, message)
   }
 }
 
@@ -37,36 +58,39 @@ function onMessageHandler (target, context, msg, self) {
 
   // This isn't a command since it has no prefix:
   if (msg.substr(0, 1) !== commandPrefix) {
-    console.log(`[${target} (${context['message-type']})] ${context.username}: ${msg}`)
+
+    // This console log is only for logging message. ONLY FOR DEBUGGING
+    //console.log(`[${target} (${context['message-type']})] ${context.username}: ${msg}`)
     return
   }
 
   // Split the message into individual words:
-  const parse = msg.slice(1).split(' ')
+  const parse = msg.slice(1).split(' ');
   // The command name is the first (0th) one:
-  const commandName = parse[0]
+  const commandName = parse[0];
   // The rest (if any) are the parameters:
-  const params = parse.splice(1)
+  const params = parse.splice(1);
 
   // If the command is known, let's execute it:
   if (commandName in knownCommands) {
     // Retrieve the function by its name:
     const command = knownCommands[commandName]
     // Then call the command with parameters:
-    command(target, context, params)
-    console.log(`* Executed ${commandName} command for ${context.username}`)
+    command(target, context, params);
+    console.log(`* Executed ${commandName} command for ${context.username}`);
   } else {
-    console.log(`* Unknown command ${commandName} from ${context.username}`)
+    console.log(`* Unknown command ${commandName} from ${context.username}`);
   }
 }
 
 // Called every time the bot connects to Twitch chat:
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`)
+  console.log(`* Connected to ${addr}:${port}`);
+  client.say(opts.channels[0], '/me walks in and stares at everyone...');
 }
 
 // Called every time the bot disconnects from Twitch:
 function onDisconnectedHandler (reason) {
-  console.log(`Womp womp, disconnected: ${reason}`)
+  console.log(`Womp womp, disconnected: ${reason}`);
 }
 
